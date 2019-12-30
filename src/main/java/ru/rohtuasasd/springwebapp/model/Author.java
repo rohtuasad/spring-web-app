ccdf504b0026bdc19b8fa2dce6c0bbb7d657b966
Alexey <rohtuasad@gmail.com>
2019-12-31 00:33:18 +0300

    Add equals and hashcode

--- src/main/java/ru/rohtuasasd/springwebapp/model/Author.java
+++ src/main/java/ru/rohtuasasd/springwebapp/model/Author.java
@@ -2,6 +2,7 @@ package ru.rohtuasasd.springwebapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -26,6 +27,19 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     public String getFirstName() {
--- src/main/java/ru/rohtuasasd/springwebapp/model/Book.java
+++ src/main/java/ru/rohtuasasd/springwebapp/model/Book.java
@@ -2,6 +2,7 @@ package ru.rohtuasasd.springwebapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -31,6 +32,19 @@ public class Book {
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     public String getTitle() {
