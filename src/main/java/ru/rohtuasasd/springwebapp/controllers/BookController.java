aabee5867acef0d8727cc2b586826261a679eb74
Alexey <rohtuasad@gmail.com>
2020-01-01 22:00:02 +0300

    Add book controller

---
+++ src/main/java/ru/rohtuasasd/springwebapp/controllers/BookController.java
@@ -0,0 +1,21 @@
+package ru.rohtuasasd.springwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.rohtuasasd.springwebapp.repository.BookRepository;
+
+@Controller
+public class BookController {
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
