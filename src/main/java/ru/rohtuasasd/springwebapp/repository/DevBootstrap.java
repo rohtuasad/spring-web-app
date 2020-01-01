1838bc4ef7ce78b5f12918e298567fc845215494
Alexey <rohtuasad@gmail.com>
2020-01-01 18:03:46 +0300

    Add dev data initialization

---
+++ src/main/java/ru/rohtuasasd/springwebapp/repository/DevBootstrap.java
@@ -0,0 +1,41 @@
+package ru.rohtuasasd.springwebapp.repository;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import ru.rohtuasasd.springwebapp.model.Author;
+import ru.rohtuasasd.springwebapp.model.Book;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE development without EJB", "23444", "Worx");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
